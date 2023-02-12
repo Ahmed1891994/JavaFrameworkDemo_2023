33b610230891563d9098cdb94e7943c7f8ecf619
Ahmed1891994 <ahmedhakem189@yahoo.com>
2023-02-12 22:27:53 +0200

    Edit class ipAddress

--- src/main/java/utils/ListenToIPAddress.java
+++ src/main/java/utils/ListenToIPAddress.java
@@ -5,9 +5,11 @@ import java.net.UnknownHostException;
 public class ListenToIPAddress {
 	public String GetMyIpAddress() throws UnknownHostException
 	{
-		String result = System.getenv("HUB_HOST");
-		if(result != null)
+		String result = System.getProperty("HUB_HUB");
+		System.out.print(">>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>"+result);
+		if(result == null)
 			result = InetAddress.getLocalHost().getHostAddress();
+		System.out.print("XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX"+result);
 		return result;
 	}
 }

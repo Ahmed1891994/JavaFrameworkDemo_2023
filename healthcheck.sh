b8af00cd0c42887b53a8e7702ae16a965077de59
Ahmed1891994 <ahmedhakem189@yahoo.com>
2023-02-12 02:33:57 +0200

    edit healthcheck

--- healthcheck.sh
+++ healthcheck.sh
@@ -4,6 +4,7 @@
 # MODULE
 
 echo "Checking if hub is ready - 192.168.1.4"
+echo "$localhost"
 
 while [ "$( curl -s http://192.168.1.4:4444/wd/hub/status | jq -r .value.ready )" != "true" ]
 do
@@ -12,6 +13,5 @@ done
 
 # start the java command
 java -cp JavaFrameworkDemo-0.0.1-SNAPSHOT.jar:JavaFrameworkDemo-0.0.1-SNAPSHOT-tests.jar:libs/* \
-    -DHUBHOST=192.168.1.4 \
-    -Denv=prod \
-    org.testng.TestNG $MODULE
+    -DHUBHOST=192.168.1.4 -Denv=prod org.testng.TestNG $MODULE
+    

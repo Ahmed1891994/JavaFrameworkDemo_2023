bdff235858f286fd0cf870fab7cb90d3e2967e53
Ahmed1891994 <ahmedhakem189@yahoo.com>
2023-02-13 14:54:44 +0200

    .

--- src/main/java/utils/ListenToIPAddress.java
+++ src/main/java/utils/ListenToIPAddress.java
@@ -5,7 +5,7 @@ import java.net.UnknownHostException;
 public class ListenToIPAddress {
 	public String GetMyIpAddress() throws UnknownHostException
 	{
-		String result = System.getProperty("HUB_HUB");
+		String result = System.getProperty("HUBHOST");
 		if(result == null)
 			result = InetAddress.getLocalHost().getHostAddress();
 		return result;

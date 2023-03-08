82db86ce2971608e0615c0a9917c6ee20d7a8bd3
Ahmed1891994 <ahmedhakem189@yahoo.com>
2023-03-08 14:22:14 +0200

    Update healthcheck.sh

--- healthcheck.sh
+++ healthcheck.sh
@@ -7,8 +7,10 @@ echo "Checking if hub is ready - $HUBHOST"
 
 while [ "$( curl -s http://$HUBHOST:4444/wd/hub/status | jq -r .value.ready )" != "true" ]
 do
-	sleep 1000000
+	sleep 1
 done
+
+sleep 1000000
 
 # start the java command
 java -cp framework_jar.jar:framework_jar-tests.jar:libs/* -DHUBHOST=$HUBHOST -Denv=prod org.testng.TestNG $MODULE
